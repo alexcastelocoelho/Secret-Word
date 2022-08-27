f007ffa629df31c8b344e06492f3a9b670a57c52
Alex Castelo <alexcastelo437@gmail.com>
2022-08-27 12:03:00 -0300

    feat: importing states, word list and game and gameover components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-
 import './App.css';
+
+import { useState, useEffect, useCallback } from 'react';
+
+import { wordsList } from './data/Words';
+
 import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"},
+]
 
 function App() {
+
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList)
+
+  console.log(words)
+ 
   return (
     <div className="App">
-      <StartScreen/>
+      {gameStage === "start" && <StartScreen />}
+      {gameStage === "game" && <Game />}
+      {gameStage === "end" && <GameOver />}
     </div>
   );
 }
