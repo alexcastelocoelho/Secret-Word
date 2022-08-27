96b1ce166473669aee040185ec9483e07ee540bd
Alex Castelo <alexcastelo437@gmail.com>
2022-08-27 11:22:10 -0300

    feat: creation of the startscreen component and its style

---
+++ src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import "./StartScreen.css"
+
+const StartScreen = () => {
+  return (
+    <div className="start">
+        <h1>Secret Word</h1>
+        <p>click the button below to start playing</p>
+        <button>start the game</button>
+    </div>
+  )
+}
+
+export default StartScreen
