75c66fa2241ef8e9e08de44c95256cb01a9eeed9
Alex Castelo <alexcastelo437@gmail.com>
2022-08-27 15:27:02 -0300

    feat: created jsx structure and style for game

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,9 +2,31 @@ import "./Game.css"
 
 const Game = ({verifyLetter}) => {
   return (
-    <div>
-        <h1>Game</h1>
-        <button onClick={verifyLetter}>end game</button>
+    <div className="game">
+        <p className="points">
+            <span>punctuation: 000</span>
+        </p>
+        <h1>guess the word</h1>
+        <h3 className="tip">
+        hint about the word <span>tip...</span>
+        </h3>
+        <p>you still have xxx tries</p>
+        <div className="wordContainer">
+            <span className="letter"></span>
+            <span className="blankSquare" ></span>
+        </div>
+        <div className="letterContainer">
+            <p>try to guess a letter of the word:</p>
+            <form >
+                <input type="text" name="letter" maxLength="1" required />
+                <button>play!</button>
+            </form>
+        </div>
+        <div className="wrongLettersContainer">
+            <p>letters already used:</p>
+            <span>a,</span>
+            <span>b,</span>
+        </div>
     </div>
   )
 }
