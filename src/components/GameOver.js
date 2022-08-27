ce9e9cf7b3805983d0eafde873afa42251713c7e
Alex Castelo <alexcastelo437@gmail.com>
2022-08-27 18:12:19 -0300

    feat: created jsx structure and style for gameover

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,9 +1,10 @@
 import "./GameOver.css"
 
-const GameOver = ({retry}) => {
+const GameOver = ({retry, score}) => {
   return (
     <div>
-        <h1>Game Over</h1>
+        <h1>End Game!</h1>
+        <h2>your score was: <span>{score}</span></h2>
         <button onClick={retry}>reset game</button>
     </div>
   )
